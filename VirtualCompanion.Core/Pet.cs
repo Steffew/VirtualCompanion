66d3673470392dfed07bc9d871c0dcbf0f33801d
Steffew <xgenyofficial@gmail.com>
2024-04-23 10:23:16 +0200

    Add Pet class

--- VirtualCompanion.Core/Pet.cs
+++ VirtualCompanion.Core/Pet.cs
@@ -1,21 +1,21 @@
 ﻿namespace VirtualCompanion.Core
 {
-    public class Item
+    public class Pet
     {
         public int Id { get; private set; }
         public string? Name { get; private set; }
-        public int Cost { get; private set; }
+        public float Health { get; private set; }
         public float Experience { get; private set; }
         public float Energy { get; private set; }
         public float Mood { get; private set; }
         public float Hunger { get; private set; }
         public float Hygiene { get; private set; }
 
-        public Item(int id, string name, int cost, float experience, float energy, float mood, float hunger, float hygiene)
+        public Pet(int id, string name, float health, float experience, float energy, float mood, float hunger, float hygiene)
         {
             Id = id;
             Name = name;
-            Cost = cost;
+            Health = health;
             Experience = experience;
             Energy = energy;
             Mood = mood;
