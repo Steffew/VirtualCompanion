d822ef6af84ed9e98091d7e0fc9a95131d554022
Steffew <xgenyofficial@gmail.com>
2024-05-08 21:01:44 +0200

    Create IItemRepository.cs

---
+++ VirtualCompanion.Core/Interfaces/IItemRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace VirtualCompanion.Core.Interfaces
+{
+    public interface IItemRepository
+    {
+        List<Pet> GetAllItems();
+        Pet GetItem(int id);
+        bool UpdateItem(Item item);
+        bool DeleteItem(Item item);
+    }
+}
