9073680f89418e72d2219a0bfc71b5625ffce921
Steffew <xgenyofficial@gmail.com>
2024-05-13 22:51:19 +0200

    Create PetviewModel.cs

---
+++ VirtualCompanion/Models/PetviewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace VirtualCompanion.Models
+{
+    public class PetviewModel
+    {
+        public int OwnerId { get; set; }
+        public float Health { get; set; }
+        public float Experience { get; set; }
+        public float Energy { get; set; }
+        public float Mood { get; set; }
+        public float Hunger { get; set; }
+        public float Hygiene { get; set; }
+    }
+}
