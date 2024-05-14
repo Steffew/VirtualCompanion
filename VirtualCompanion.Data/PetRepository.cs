0017032c8b558f42844f617def139b10020b12d8
Steffew <xgenyofficial@gmail.com>
2024-05-14 11:51:21 +0200

    Add unimplemented PetRepository

---
+++ VirtualCompanion.Data/PetRepository.cs
@@ -0,0 +1,35 @@
+﻿using VirtualCompanion.Core.Entities;
+using VirtualCompanion.Core.Interfaces;
+
+namespace VirtualCompanion.Data
+{
+    internal class PetRepository : IPetRepository
+    {
+        private readonly string _connectionString;
+
+        public PetRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        bool IPetRepository.DeletePet(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Pet> IPetRepository.GetAllPets()
+        {
+            throw new NotImplementedException();
+        }
+
+        Pet IPetRepository.GetPet(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IPetRepository.UpdatePet(Pet pet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
