107ad31d53952c8aee6f36702d19b70cdd78e129
Steffew <xgenyofficial@gmail.com>
2024-05-14 11:41:07 +0200

    Add first class of ItemRepository

--- VirtualCompanion.Data/Class1.cs
+++ VirtualCompanion.Data/Class1.cs
@@ -1,7 +1,41 @@
-﻿namespace VirtualCompanion.Data
-{
-    public class Class1
-    {
+﻿using MySql.Data.MySqlClient;
+using VirtualCompanion.Core.Entities;
+using VirtualCompanion.Core.Interfaces;
 
+namespace VirtualCompanion.Data
+{
+    public class ItemRepository : IItemRepository
+    {
+        private readonly string _connectionString;
+
+        public ItemRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Item> GetAllItems()
+        {
+            var items = new List<Item>();
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                var cmd = new MySqlCommand("SELECT * FROM item", conn);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        items.Add(new Item(
+                            reader.GetInt32("id"),
+                            reader.GetInt32("cost"),
+                            reader.GetFloat("experience"),
+                            reader.GetFloat("energy"),
+                            reader.GetFloat("mood"),
+                            reader.GetFloat("hunger"),
+                            reader.GetFloat("hygiene")));
+                    }
+                }
+            }
+            return items;
+        }
     }
 }
