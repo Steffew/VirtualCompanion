680f3520d1c89ce04945d44513a9756e7fd7661c
Steffew <xgenyofficial@gmail.com>
2024-05-14 11:47:32 +0200

    Change name of script

---
+++ VirtualCompanion.Data/ItemRepository.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using VirtualCompanion.Core.Entities;
+using VirtualCompanion.Core.Interfaces;
+
+namespace VirtualCompanion.Data
+{
+    public class ItemRepository : IItemRepository
+    {
+        private readonly string _connectionString;
+
+        public ItemRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Item> GetAllItems()
+        {
+            var items = new List<Item>();
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                var cmd = new MySqlCommand("SELECT * FROM item", conn);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        items.Add(new Item(
+                            reader.GetInt32("id"),
+                            reader.GetString("name"),
+                            reader.GetInt32("cost"),
+                            reader.GetFloat("experience"),
+                            reader.GetFloat("energy"),
+                            reader.GetFloat("mood"),
+                            reader.GetFloat("hunger"),
+                            reader.GetFloat("hygiene")));
+                    }
+                }
+            }
+            return items;
+        }
+
+        public Item GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+        public bool DeleteItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
