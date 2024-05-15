7bbf82e02d91ad0c96596b1ac8bdf831f34d5afe
Steffew <xgenyofficial@gmail.com>
2024-05-15 21:51:49 +0200

    Remove id parameter

--- VirtualCompanion.Core/Entities/Pet.cs
+++ VirtualCompanion.Core/Entities/Pet.cs
@@ -2,9 +2,8 @@
 {
     public class Pet
     {
-        public int Id { get; private set; }
-        public string? Name { get; private set; }
         public int OwnerId { get; set; }
+        public string? Name { get; private set; }
         public float Health { get; private set; }
         public float Experience { get; private set; }
         public float Energy { get; private set; }
@@ -12,9 +11,8 @@
         public float Hunger { get; private set; }
         public float Hygiene { get; private set; }
 
-        public Pet(int id, int ownerId, string name, float health, float experience, float energy, float mood, float hunger, float hygiene)
+        public Pet(int ownerId, string name, float health, float experience, float energy, float mood, float hunger, float hygiene)
         {
-            Id = id;
             OwnerId = ownerId;
             Name = name;
             Health = health;
