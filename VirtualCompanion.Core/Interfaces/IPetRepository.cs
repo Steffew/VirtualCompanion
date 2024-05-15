0c9e9b0f23e750a3415a320466756b06cec85566
Steffew <xgenyofficial@gmail.com>
2024-05-15 22:46:26 +0200

    Add GetAllPetsByOwnerId method to interface

--- VirtualCompanion.Core/Interfaces/IPetRepository.cs
+++ VirtualCompanion.Core/Interfaces/IPetRepository.cs
@@ -5,6 +5,7 @@ namespace VirtualCompanion.Core.Interfaces
     public interface IPetRepository
     {
         List<Pet> GetAllPets();
+        List<Pet> GetAllPetsByOwnerId(int ownerId);
         Pet GetPet(int id);
         bool UpdatePet(Pet pet);
         bool DeletePet(int id);
