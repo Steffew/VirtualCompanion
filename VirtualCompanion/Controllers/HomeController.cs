037579afea6164403ca19de822a267a7be4207c0
Steffew <xgenyofficial@gmail.com>
2024-05-15 23:39:30 +0200

    Update HomeController.cs

--- VirtualCompanion/Controllers/HomeController.cs
+++ VirtualCompanion/Controllers/HomeController.cs
@@ -7,36 +7,14 @@ namespace VirtualCompanion.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IPetRepository _petRepository;
-
-        public HomeController(IPetRepository petRepository)
+        public HomeController()
         {
-            _petRepository = petRepository;
         }
 
         public IActionResult Index()
         {
-            var petsData = _petRepository.GetAllPetsByOwnerId(1); // OwnerId is hard-coded for now, login system not implemented (yet).
-            var petViewModels = petsData.Select(pet => new PetViewModel
-            {
-                OwnerId = pet.OwnerId,
-                Name = pet.Name,
-                Health = pet.Health,
-                Experience = pet.Experience,
-                Energy = pet.Energy,
-                Mood = pet.Mood,
-                Hunger = pet.Hunger,
-                Hygiene = pet.Hygiene
-            }).ToList();
-
-            var model = new PetListViewModel
-            {
-                Pets = petViewModels
-            };
-
-            return View(model);
+            return View();
         }
-
 
         public IActionResult Privacy()
         {
