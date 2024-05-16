fc658330cb7ff5d84ad90d7bd7cc3d95aba54c86
Steffew <xgenyofficial@gmail.com>
2024-05-16 12:38:20 +0200

    Add Add method

--- VirtualCompanion.Core/Interfaces/IPetRepository.cs
+++ VirtualCompanion.Core/Interfaces/IPetRepository.cs
@@ -6,6 +6,7 @@ namespace VirtualCompanion.Core.Interfaces
     {
         List<Pet> GetAll();
         List<Pet> GetAll(int ownerId);
+        void Add(Pet pet);
         Pet GetPet(int id);
         bool Update(Pet pet);
         bool Delete(int id);
