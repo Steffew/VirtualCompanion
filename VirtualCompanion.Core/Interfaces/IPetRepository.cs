c9f6746327b8c7e443f7bbe87481b9a0eeb95dcd
Steffew <xgenyofficial@gmail.com>
2024-05-16 12:47:30 +0200

    Change method name

--- VirtualCompanion.Core/Interfaces/IPetRepository.cs
+++ VirtualCompanion.Core/Interfaces/IPetRepository.cs
@@ -7,7 +7,7 @@ namespace VirtualCompanion.Core.Interfaces
         List<Pet> GetAll();
         List<Pet> GetAll(int ownerId);
         void Add(Pet pet);
-        Pet GetPet(int id);
+        Pet Get(int id);
         bool Update(Pet pet);
         bool Delete(int id);
     }
