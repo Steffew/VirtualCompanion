8fe90f95d7260710201823babcd26f5fbf3ff0dc
Steffew <xgenyofficial@gmail.com>
2024-05-17 17:50:52 +0200

    Create Owner.cs

---
+++ VirtualCompanion.Core/Entities/Owner.cs
@@ -0,0 +1,16 @@
+﻿namespace VirtualCompanion.Core.Entities
+{
+    public class Owner
+    {
+        public int Id { get; private set; }
+        public int Balance { get; private set; }
+        public int PetCapacity { get; private set; }
+
+        public Owner(int id, int balance, int petCapacity)
+        {
+            Id = id;
+            Balance = balance;
+            PetCapacity = petCapacity;
+        }
+    }
+}
