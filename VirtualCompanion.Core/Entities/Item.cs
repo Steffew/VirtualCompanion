288b81c126929b2d003d85c88ccc46de1b7fed2b
Steffew <xgenyofficial@gmail.com>
2024-05-17 18:21:29 +0200

    Add health attribute to Item entity

--- VirtualCompanion.Core/Entities/Item.cs
+++ VirtualCompanion.Core/Entities/Item.cs
@@ -5,13 +5,14 @@
         public int Id { get; private set; }
         public string? Name { get; private set; }
         public int Cost { get; private set; }
-        public float Experience { get; private set; }
+        public float Health { get; private set; }
+        public float Hunger { get; private set; }
         public float Energy { get; private set; }
         public float Mood { get; private set; }
-        public float Hunger { get; private set; }
         public float Hygiene { get; private set; }
+        public float Experience { get; private set; }
 
-        public Item(int id, string name, int cost, float experience, float energy, float mood, float hunger, float hygiene)
+        public Item(int id, string name, float health, int cost, float experience, float energy, float mood, float hunger, float hygiene)
         {
             if (string.IsNullOrWhiteSpace(name))
                 throw new ArgumentException("Name cannot be empty or null.", nameof(name));
@@ -24,6 +25,7 @@
             Cost = cost;
             Experience = experience;
             Energy = energy;
+            Health = health;
             Mood = mood;
             Hunger = hunger;
             Hygiene = hygiene;
