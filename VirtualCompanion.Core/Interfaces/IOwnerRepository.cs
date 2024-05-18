6040efab6160a4c3ec5644c6277cb930f52a3a64
Steffew <xgenyofficial@gmail.com>
2024-05-18 13:16:58 +0200

    Create IOwnerRepository.cs

---
+++ VirtualCompanion.Core/Interfaces/IOwnerRepository.cs
@@ -0,0 +1,12 @@
+﻿using VirtualCompanion.Core.Entities;
+
+namespace VirtualCompanion.Core.Interfaces
+{
+    internal interface IOwnerRepository
+    {
+        List<Owner> GetAll();
+        Item Get(int id);
+        bool Update(Owner owner);
+        bool Delete(int id);
+    }
+}
