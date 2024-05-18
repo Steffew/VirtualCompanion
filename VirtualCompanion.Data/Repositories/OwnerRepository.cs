ae8d52ed9aa1ea630635a0cd0ca8f42ac45e6550
Steffew <xgenyofficial@gmail.com>
2024-05-18 13:27:13 +0200

    Implement GetAll method

--- VirtualCompanion.Data/Repositories/OwnerRepository.cs
+++ VirtualCompanion.Data/Repositories/OwnerRepository.cs
@@ -1,4 +1,8 @@
-﻿namespace VirtualCompanion.Data.Repositories
+﻿using MySql.Data.MySqlClient;
+using VirtualCompanion.Core.Entities;
+using VirtualCompanion.Core.Interfaces;
+
+namespace VirtualCompanion.Data.Repositories
 {
     public class OwnerRepository : IOwnerRepository
     {
@@ -7,6 +11,42 @@
         public OwnerRepository(string connectionString)
         {
             _connectionString = connectionString;
+        }
+
+        public List<Owner> GetAll()
+        {
+            var owners = new List<Owner>();
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                var cmd = new MySqlCommand("SELECT * FROM owner", conn);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        owners.Add(new Owner(
+                            reader.GetInt32("id"),
+                            reader.GetInt32("balance"),
+                            reader.GetInt32("petCapacity")));
+                    }
+                }
+            }
+            return owners;
+        }
+
+        public Item Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Owner owner)
+        {
+            throw new NotImplementedException();
         }
     }
 }
