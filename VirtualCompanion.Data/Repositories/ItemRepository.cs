072556b279b65d7bfb7112532eaf450fe71adee9
Steffew <xgenyofficial@gmail.com>
2024-05-18 13:08:15 +0200

    Implement Update method

--- VirtualCompanion.Data/Repositories/ItemRepository.cs
+++ VirtualCompanion.Data/Repositories/ItemRepository.cs
@@ -66,12 +66,29 @@ namespace VirtualCompanion.Data.Repositories
                 }
             }
             return item;
-        }   
-
-        public bool Update(Item item)
-        {
-            throw new NotImplementedException();
         }
+
+        public bool Update(Pet pet)
+        {
+            int rowsAffected;
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+                var cmd = new MySqlCommand("UPDATE pet SET ownerId = @ownerId, name = @name, health = @health, experience = @experience, energy = @energy, mood = @mood, hunger = @hunger, hygiene = @hygiene WHERE id = @id", conn);
+                cmd.Parameters.AddWithValue("@id", pet.Id);
+                cmd.Parameters.AddWithValue("@ownerId", pet.OwnerId);
+                cmd.Parameters.AddWithValue("@name", pet.Name);
+                cmd.Parameters.AddWithValue("@health", pet.Health);
+                cmd.Parameters.AddWithValue("@experience", pet.Experience);
+                cmd.Parameters.AddWithValue("@energy", pet.Energy);
+                cmd.Parameters.AddWithValue("@mood", pet.Mood);
+                cmd.Parameters.AddWithValue("@hunger", pet.Hunger);
+                cmd.Parameters.AddWithValue("@hygiene", pet.Hygiene);
+                rowsAffected = cmd.ExecuteNonQuery();
+            }
+            return rowsAffected > 0;
+        }
+
         public bool Delete(int id)
         {
             int rowsAffected;
