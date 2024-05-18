2cc617cf11694bdc4135c43f909c7a31656a3801
Steffew <xgenyofficial@gmail.com>
2024-05-18 13:42:57 +0200

    Add Id attribute to PetViewModel

--- VirtualCompanion/Models/PetviewModel.cs
+++ VirtualCompanion/Models/PetviewModel.cs
@@ -2,6 +2,7 @@
 {
     public class PetViewModel
     {
+        public int Id { get; set; }
         public int OwnerId { get; set; }
         public string Name { get; set; }
         public float Health { get; set; }
