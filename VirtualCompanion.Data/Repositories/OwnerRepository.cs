c90889161ec25860476503d0d65c45c2742043c3
Steffew <xgenyofficial@gmail.com>
2024-05-18 13:23:53 +0200

    Create OwnerRepository.cs

---
+++ VirtualCompanion.Data/Repositories/OwnerRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace VirtualCompanion.Data.Repositories
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly string _connectionString;
+
+        public OwnerRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+    }
+}
