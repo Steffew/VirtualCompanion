92afaa235fbdf4a07eea9568895953db1c5039ee
Steffew <xgenyofficial@gmail.com>
2024-05-19 15:53:50 +0200

    Create IPetService.cs

---
+++ VirtualCompanion.Core/Interfaces/IPetService.cs
@@ -0,0 +1,16 @@
+﻿using VirtualCompanion.Core.Entities;
+using System.Collections.Generic;
+
+namespace VirtualCompanion.Core.Interfaces
+{
+    public interface IPetService
+    {
+        void CreatePet(Pet pet);
+        List<Pet> GetAllPets(int ownerId);
+        List<Pet> GetAllPets();
+        Pet GetPet(int ownerId);
+        void UpdatePet(Pet pet);
+        void DeletePet(int petId);
+        void ApplyItemToPet(int petId, Item item);
+    }
+}
