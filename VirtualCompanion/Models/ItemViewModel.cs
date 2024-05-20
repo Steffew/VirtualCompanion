b6fbe578116e6c9deaa43b56c0d171bc3bbfc45c
Steffew <xgenyofficial@gmail.com>
2024-05-20 20:50:07 +0200

    Implement ItemViewModel

---
+++ VirtualCompanion/Models/ItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace VirtualCompanion.Models
+{
+    public class ItemViewModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Cost { get; set; }
+        public float Health { get; set; }
+        public float Hunger { get; set; }
+        public float Energy { get; set; }
+        public float Mood { get; set; }
+        public float Hygiene { get; set; }
+        public float Experience { get; set; }
+    }
+
+    public class ItemListViewModel
+    {
+        public List<ItemViewModel> Pets { get; set; } = new List<ItemViewModel>();
+    }
+}
