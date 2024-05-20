3ad4079c4704618c79375e3de6b7edc005da42a1
Steffew <xgenyofficial@gmail.com>
2024-05-20 21:32:02 +0200

    Implement entity Inventory

---
+++ VirtualCompanion.Core/Entities/Inventory.cs
@@ -0,0 +1,16 @@
+﻿namespace VirtualCompanion.Core.Entities
+{
+    public class Inventory
+    {
+        public int ItemId { get; private set; }
+        public int OwnerId { get; private set; }
+        public int Quantity { get; private set; }
+
+        public Inventory(int itemId, int ownerId, int quantity)
+        {
+            ItemId = itemId;
+            OwnerId = ownerId;
+            Quantity = quantity;
+        }
+    }
+}
