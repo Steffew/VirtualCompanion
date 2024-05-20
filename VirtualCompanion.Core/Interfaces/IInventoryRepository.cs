eb95598f6d99faf91327e54e45593c9a00cb6282
Steffew <xgenyofficial@gmail.com>
2024-05-20 22:08:20 +0200

    Implement IInventoryRepository

---
+++ VirtualCompanion.Core/Interfaces/IInventoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using VirtualCompanion.Core.Entities;
+
+namespace VirtualCompanion.Core.Interfaces
+{
+    public interface IInventoryRepository
+    {
+        List<Inventory> GetAll(int ownerId);
+        void Add(Inventory inventory);
+        bool Update(Inventory inventory);
+        bool Delete(int ownerId, int itemId);
+    }
+}
