945d858ad2c13ee5cc42301984fd7c4e17b2cd40
Steffew <xgenyofficial@gmail.com>
2024-05-20 22:35:26 +0200

    Implement IInventoryService

---
+++ VirtualCompanion.Core/Interfaces/IInventoryService.cs
@@ -0,0 +1,13 @@
+﻿using VirtualCompanion.Core.Entities;
+
+namespace VirtualCompanion.Core.Interfaces
+{
+    public interface IInventoryService
+    {
+        List<Inventory> GetInventoriesByOwnerId(int ownerId);
+        void AddItemToInventory(Inventory inventory);
+        bool UpdateInventoryItem(Inventory inventory);
+        bool RemoveItemFromInventory(int ownerId, int itemId);
+        void UseItem(int ownerId, int itemId, int quantity);
+    }
+}
