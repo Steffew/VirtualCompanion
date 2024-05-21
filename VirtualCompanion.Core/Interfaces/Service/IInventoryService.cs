db32fa42345eeb78e143e7f504bb8bfa4b658422
Steffew <xgenyofficial@gmail.com>
2024-05-21 23:48:56 +0200

    Add GetInventoryByOwnerIdAndItemId to interface

--- VirtualCompanion.Core/Interfaces/Service/IInventoryService.cs
+++ VirtualCompanion.Core/Interfaces/Service/IInventoryService.cs
@@ -5,6 +5,7 @@ namespace VirtualCompanion.Core.Interfaces.Service
     public interface IInventoryService
     {
         List<Inventory> GetInventoriesByOwnerId(int ownerId);
+        Inventory GetInventoryByOwnerIdAndItemId(int ownerId, int itemId);
         void AddItemToInventory(Inventory inventory);
         bool UpdateInventoryItem(Inventory inventory);
         bool RemoveItemFromInventory(int ownerId, int itemId);
