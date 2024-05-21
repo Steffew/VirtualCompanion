73d191511db3a836f2ff85c8443b0c1e3d8cc103
Steffew <xgenyofficial@gmail.com>
2024-05-21 23:48:45 +0200

    Add itemName to Inventory class

--- VirtualCompanion.Core/Entities/Inventory.cs
+++ VirtualCompanion.Core/Entities/Inventory.cs
@@ -5,12 +5,14 @@
         public int ItemId { get; private set; }
         public int OwnerId { get; private set; }
         public int Quantity { get; private set; }
+        public string ItemName { get; set; }
 
-        public Inventory(int itemId, int ownerId, int quantity)
+        public Inventory(int itemId, int ownerId, int quantity, string itemName = "Item")
         {
             ItemId = itemId;
             OwnerId = ownerId;
             Quantity = quantity;
+            ItemName = itemName;
         }
 
         public void AdjustQuantity(int amount)
