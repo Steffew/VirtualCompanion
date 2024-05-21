99b40ce14876bd679d900143cae9e8f58605e894
Steffew <xgenyofficial@gmail.com>
2024-05-21 23:49:07 +0200

    Create InventoryController.cs

---
+++ VirtualCompanion/Controllers/InventoryController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VirtualCompanion.Core.Interfaces.Service;
+using VirtualCompanion.Models;
+using System.Linq;
+
+public class InventoryController : Controller
+{
+    private readonly IInventoryService _inventoryService;
+
+    public InventoryController(IInventoryService inventoryService)
+    {
+        _inventoryService = inventoryService;
+    }
+
+    [HttpGet]
+    [Route("Inventory/Owner/{ownerId}")]
+    public IActionResult OwnerInventory(int ownerId)
+    {
+        var inventoryItems = _inventoryService.GetInventoriesByOwnerId(ownerId);
+        var model = new ItemListViewModel
+        {
+            Items = inventoryItems.Select(i => new ItemViewModel
+            {
+                Id = i.ItemId,
+                Name = i.ItemName,
+                Quantity = i.Quantity,
+            }).ToList()
+        };
+        return View(model);
+    }
+
+    [HttpGet]
+    [Route("Inventory/Details/{ownerId}/{itemId}")]
+    public IActionResult Details(int ownerId, int itemId)
+    {
+        var inventoryItem = _inventoryService.GetInventoryByOwnerIdAndItemId(ownerId, itemId);
+        if (inventoryItem == null)
+            return NotFound();
+
+        var viewModel = new ItemViewModel
+        {
+            Id = inventoryItem.ItemId,
+            OwnerId = inventoryItem.OwnerId,
+            Quantity = inventoryItem.Quantity,
+            Name = inventoryItem.ItemName
+        };
+
+        return View("Details", viewModel);
+    }
+}
