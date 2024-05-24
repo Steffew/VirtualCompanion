def5e08425bbb7012efd00879922bd331ef48ba5
Steffew <xgenyofficial@gmail.com>
2024-05-24 22:32:32 +0200

    Implement Get_all_by_wrong_ID

--- VirtualCompanion.Tests/InventoryTests.cs
+++ VirtualCompanion.Tests/InventoryTests.cs
@@ -4,7 +4,7 @@ namespace VirtualCompanion.Test
     public class InventoryTests
     {
         [Fact]
-        public void Get_all_inventory_by_ID()
+        public void Get_all_by_correct_ID()
         {
             Inventory inventory1 = new Inventory(itemId:1, ownerId:1, amount:1);
             Inventory inventory2 = new Inventory(itemId:2, ownerId:1, amount:1);
@@ -19,6 +19,20 @@ namespace VirtualCompanion.Test
             InventoryService inventoryService = new InventoryService(mockRepository);
 
             Assert.Equal(3, inventoryService.GetInventoriesByOwnerId(1).Count);
+        }
+
+        [Fact]
+        public void Get_all_by_wrong_ID()
+        {
+            Inventory inventory1 = new Inventory(itemId: 1, ownerId: 2, amount: 1);
+
+            List<Inventory> inventoryList = new List<Inventory>();
+            inventoryList.Add(inventory1);
+
+            MockInventoryRepository mockRepository = new MockInventoryRepository(inventoryList);
+            InventoryService inventoryService = new InventoryService(mockRepository);
+
+            Assert.Equal(0, inventoryService.GetInventoriesByOwnerId(1).Count);
         }
     }
 }
