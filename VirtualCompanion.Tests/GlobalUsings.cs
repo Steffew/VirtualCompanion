fabc703e2341db5065c464197cdc9da9484f83e1
Steffew <xgenyofficial@gmail.com>
2024-05-24 22:29:03 +0200

    Update GlobalUsings.cs

--- VirtualCompanion.Tests/GlobalUsings.cs
+++ VirtualCompanion.Tests/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using VirtualCompanion.Core.Entities;
 global using VirtualCompanion.Core.Services;
 global using VirtualCompanion.Core.Interfaces.Service;
 global using VirtualCompanion.Core.Interfaces.Repository;
+global using VirtualCompanion.Test.MockRepositories;
