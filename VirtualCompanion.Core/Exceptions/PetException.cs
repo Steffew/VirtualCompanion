d7d8f0325d25eaf1c90ffca1945edfb53cf30ecb
Steffew <xgenyofficial@gmail.com>
2024-05-30 10:29:00 +0200

    Create PetException.cs

---
+++ VirtualCompanion.Core/Exceptions/PetException.cs
@@ -0,0 +1,15 @@
+﻿namespace VirtualCompanion.Core.Exceptions
+{
+    public class PetException : Exception
+    {
+        public PetException(string message) : base(message)
+        {
+
+        }
+
+        public PetException(string message, Exception innerException) : base(message,innerException)
+        {
+
+        }
+    }
+}
