def695a11bf882c10aec0530db45cc21794ac2c1
Steffew <xgenyofficial@gmail.com>
2024-05-30 14:52:57 +0200

    Add _throwException parameter

--- VirtualCompanion.Tests/MockRepositories/MockInventoryRepository.cs
+++ VirtualCompanion.Tests/MockRepositories/MockInventoryRepository.cs
@@ -1,33 +1,57 @@
 ﻿using VirtualCompanion.Core.Entities;
+using VirtualCompanion.Core.Exceptions;
+using Xunit.Sdk;
 
 namespace VirtualCompanion.Test.MockRepositories
 {
     public class MockInventoryRepository : IInventoryRepository
     {
         private List<Inventory> _inventories = new List<Inventory>();
-        // todo: Test ALL outcomes, even exceptions from the service class, add params to methods to test exceptions
-        public MockInventoryRepository(List<Inventory> inventories)
+        private bool _throwException;
+
+        public MockInventoryRepository(List<Inventory> inventories, bool throwException = false)
         {
             _inventories = inventories;
+            _throwException = throwException;
         }
 
         public List<Inventory> GetAll(int ownerId)
         {
+            if (_throwException)
+            {
+                throw new InventoryException("Failed to connect to the database.");
+            }
+
             return _inventories.Where(inventory => inventory.OwnerId == ownerId).ToList();
         }
 
         public Inventory GetByOwnerIdAndItemId(int ownerId, int itemId)
         {
+            if (_throwException)
+            {
+                throw new InventoryException("Failed to connect to the database.");
+            }
+
             return _inventories.FirstOrDefault(inventory => inventory.OwnerId == ownerId && inventory.ItemId == itemId);
         }
 
         public void Add(Inventory inventory)
         {
+            if (_throwException)
+            {
+                throw new InventoryException("Failed to connect to the database.");
+            }
+
             _inventories.Add(inventory);
         }
 
         public bool Update(Inventory inventory)
         {
+            if (_throwException)
+            {
+                throw new InventoryException("Failed to connect to the database.");
+            }
+
             Inventory existingInventory = GetByOwnerIdAndItemId(inventory.OwnerId, inventory.ItemId);
             if (existingInventory != null)
             {
@@ -45,6 +69,11 @@ namespace VirtualCompanion.Test.MockRepositories
 
         public bool Delete(int ownerId, int itemId)
         {
+            if (_throwException)
+            {
+                throw new InventoryException("Failed to connect to the database.");
+            }
+
             var inventory = GetByOwnerIdAndItemId(ownerId, itemId);
             if (inventory != null)
             {
